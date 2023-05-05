53e9191770feb45d1a11f60c9ddeb92fe961d8c4
Karel Tamayo <karel.tamayo@theorem.co>
2023-05-05 11:57:28 -0300

    Add explanation to the reply

--- gladiator/models/reply.py
+++ gladiator/models/reply.py
@@ -1,8 +1,9 @@
 class Reply:
-    def __init__(self, number: int, answer: str, confidence: float = 0):
+    def __init__(self, number: int, answer: str, confidence: float = 0, explanation: str = None):
         self.number = number
         self.answer = answer
         self.confidence = confidence
+        self.explanation = explanation
 
     def __repr__(self):
-        return f"Reply(number={self.number}, confidence={self.confidence}, answer={self.answer})"
+        return f"Reply(number={self.number}, confidence={self.confidence}, answer={self.answer}) | {self.explanation}"
